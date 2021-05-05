f7b4f86e8483a6dc3e2e8b7971f6f5db6228188a
Hosuqa <43502805+Hosuqa@users.noreply.github.com>
2021-05-05 12:45:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ function getQR() {
     var oro = document.getElementById('model').value;
     var boro = document.getElementById('sn').value;
     var output = document.getElementById('output');
+    var str1 = String(oro);
+    var str2 = String(boro);
+    var str3 = "https://hosuqa.github.io/praktyki2/form.html";
+    var url = str3 + "#" + str1 + "&" + str2;
     let qrcode = new QRCode(output, {
-        text: "https://hosuqa.github.io/praktyki2/form.html",
+        text: url,
         width: 277,
         height: 277,
         colorDark: "#5a8dee",
         colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.H
     });
+
 }
+
---
+++ scriptform.js
@@ -0,0 +1,12 @@
+var currentUrl = window.location.href;
+// https://hosuqa.github.io/praktyki2/form.html#zmienna&zmienna
+var modelsn = currentUrl.split("#").pop();
+// modelsn = zmienna&zmienna
+var [model, sn] = modelsn.split('&');
+
+
+
+
+
+
+
